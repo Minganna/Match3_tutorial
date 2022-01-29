664a9e1900e518449a8fca39b769e39f1fa1b9da
unknown <s4929892@bournemouth.ac.uk>
2022-01-29 17:12:06 +0000

    Started swapping logic

--- Assets/Scrpits/Gems.cs
+++ Assets/Scrpits/Gems.cs
@@ -7,6 +7,13 @@ public class Gems : MonoBehaviour
 
     Vector2Int posIndex;
     BoardManager board;
+
+    private Vector2 firstTouchPos;
+    private Vector2 finalTouchPos;
+
+    private bool mousePressed;
+    private float swipeAngle;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +23,31 @@ public class Gems : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(mousePressed && Input.GetMouseButtonUp(0))
+        {
+            mousePressed = false;
+            finalTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            calculateAngle();
+        }
+
     }
 
     public void SetUpGem(Vector2Int pos)
     {
         posIndex = pos;
         board = BoardManager.instance;
+    }
+
+    private void OnMouseDown()
+    {
+        firstTouchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePressed = true;
+    }
+
+    private void calculateAngle()
+    {
+        swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x);
+        swipeAngle = swipeAngle * 180 / Mathf.PI;
+        print(swipeAngle);
     }
 }
